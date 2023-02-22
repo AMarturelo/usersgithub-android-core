97b51e318ab8aa315461b28b8d36ffea4f49090c
Alberto <amarturelo@gmail.com>
2023-02-22 16:33:16 -0300

    change implementation to api

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,42 +24,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    testOptions {
-        unitTests.isReturnDefaultValues = true
-        unitTests.isIncludeAndroidResources = true
-        unitTests.all {
-            it.jvmArgs(
-                "-Xms2g",
-                "-Xmx2g",
-                "-XX:+DisableExplicitGC"
-            )
-            it.testLogging {
-                events("passed", "skipped", "failed", "standardOut", "standardError")
-            }
-        }
-    }
-
-    lint {
-        disable("ObsoleteLintCustomCheck", "TypographyFractions", "TypographyQuotes")
-        isAbortOnError = false
-    }
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    api("androidx.core:core-ktx:1.7.0")
+    api("androidx.appcompat:appcompat:1.4.1")
+    api("com.google.android.material:material:1.5.0")
+    api("androidx.constraintlayout:constraintlayout:2.1.3")
 
     // Coroutines
-    implementation(ApplicationDependencies.coroutinesCore)
-    implementation(ApplicationDependencies.coroutinesAndroid)
+    api(ApplicationDependencies.coroutinesCore)
+    api(ApplicationDependencies.coroutinesAndroid)
 
     //dagger
     implementation(ApplicationDependencies.dagger)
@@ -69,11 +46,5 @@ dependencies {
     kapt(ApplicationDependencies.daggerAdroidProcessor)
 
     //Logger
-    implementation(ApplicationDependencies.timber)
-
-    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).configureEach {
-        kotlinOptions {
-            freeCompilerArgs.plus("-Xjvm-default=all-compatibility")
-        }
-    }
+    api(ApplicationDependencies.timber)
 }
