0749a327e14069f49e0678be750af448879ea1f5
Alberto <amarturelo@gmail.com>
2023-02-23 11:47:29 -0300

    update version

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,6 +1,6 @@
 object ConfigData{
     const val projectName = "usersgithub-android-core"
     const val groupName = "com.amarturelo.usersgithub.android"
-    const val versionName = "1.0.4"
+    const val versionName = "1.0.5"
     const val artifactId = "core"
 }
